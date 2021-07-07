d45fe7702006021fef4c7f1eacbe11382e6fb79a
Anton Medwedew <medwedew.anton2017@yandex.ru>
2021-07-07 15:43:25 +0300

    add styles scss

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,11 @@
-
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>Hello</h1>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
